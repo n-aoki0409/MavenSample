a49c3fff7d6b78585c210236d63c81c7fd686a60
NA <n-aoki@192.168.0.100>
2016-09-11 20:57:23 +0900

    JavaDocとコメントを追加

--- src/main/java/Sample.java
+++ src/main/java/Sample.java
@@ -1,7 +1,13 @@
 
 public class Sample {
 
+	/**
+	 * method1
+	 * @param param
+	 * @return
+	 */
 	public int method1(int param) {
+		// 1の場合は1を返す
 		if (param == 1) {
 			return 1;
 		}
@@ -9,7 +15,15 @@ public class Sample {
 		return 2;
 	}
 
+	/**
+	 * method2
+	 * @param param
+	 * @return
+	 */
 	public int method2(int param) {
+		/*
+		 * 2を返す
+		 */
 		return 2;
 	}
 }
