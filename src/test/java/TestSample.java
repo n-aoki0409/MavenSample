e4e9f98e4c43f5f58553eedbdf336fc38e5060d3
NA <n-aoki@192.168.0.100>
2016-09-11 18:47:40 +0900

    if変更

--- src/test/java/TestSample.java
+++ src/test/java/TestSample.java
@@ -6,6 +6,6 @@ public class TestSample {
 
 	@Test
 	public void testMethod1_Success() {
-		Assert.assertThat(new Sample().method1(), is(1));
+		Assert.assertThat(new Sample().method1(1), is(1));
 	}
 }
