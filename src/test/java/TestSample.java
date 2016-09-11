4ca0f8f5962e41a3f7049c612933125619378c68
NA <n-aoki@192.168.0.100>
2016-09-11 15:39:46 +0900

    失敗するテストを追加

--- src/test/java/TestSample.java
+++ src/test/java/TestSample.java
@@ -5,7 +5,12 @@ import org.junit.Test;
 public class TestSample {
 
 	@Test
-	public void testMethod1() {
+	public void testMethod1_Success() {
 		Assert.assertThat(new Sample().method1(), is(1));
+	}
+
+	@Test
+	public void testMethod1_Failure() {
+		Assert.assertThat(new Sample().method1(), is(2));
 	}
 }
